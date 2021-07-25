a42f84f9e3173169509fc34c388e2ef90963cc7e
Shyrro <zsahmane@gmail.com>
2021-07-25 23:22:56 +0200

    first commit
    Signed-off-by: Shyrro <zsahmane@gmail.com>

---
+++ App.tsx
@@ -0,0 +1,145 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const Section: React.FC<{
+  title: string;
+}> = ({children, title}) => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <Text>coucou</Text>
+      <Calendar
+        // Initially visible month. Default = Date()
+        current={'2012-03-01'}
+        // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
+        minDate={'2012-05-10'}
+        // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
+        maxDate={'2012-05-30'}
+        // Handler which gets executed on day press. Default = undefined
+        onDayPress={day => {
+          console.log('selected day', day);
+        }}
+        // Handler which gets executed on day long press. Default = undefined
+        onDayLongPress={day => {
+          console.log('selected day', day);
+        }}
+        // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
+        monthFormat={'yyyy MM'}
+        // Handler which gets executed when visible month changes in calendar. Default = undefined
+        onMonthChange={month => {
+          console.log('month changed', month);
+        }}
+        // Hide month navigation arrows. Default = false
+        hideArrows={true}
+        // Replace default arrows with custom ones (direction can be 'left' or 'right')
+        renderArrow={direction => <Arrow />}
+        // Do not show days of other months in month page. Default = false
+        hideExtraDays={true}
+        // If hideArrows = false and hideExtraDays = false do not switch month when tapping on greyed out
+        // day from another month that is visible in calendar page. Default = false
+        disableMonthChange={true}
+        // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday
+        firstDay={1}
+        // Hide day names. Default = false
+        hideDayNames={true}
+        // Show week numbers to the left. Default = false
+        showWeekNumbers={true}
+        // Handler which gets executed when press arrow icon left. It receive a callback can go back month
+        onPressArrowLeft={subtractMonth => subtractMonth()}
+        // Handler which gets executed when press arrow icon right. It receive a callback can go next month
+        onPressArrowRight={addMonth => addMonth()}
+        // Disable left arrow. Default = false
+        disableArrowLeft={true}
+        // Disable right arrow. Default = false
+        disableArrowRight={true}
+        // Disable all touch events for disabled days. can be override with disableTouchEvent in markedDates
+        disableAllTouchEventsForDisabledDays={true}
+        // Replace default month and year title with custom one. the function receive a date as parameter
+        renderHeader={date => {
+          /*Return JSX*/
+        }}
+        // Enable the option to swipe between months. Default = false
+        enableSwipeMonths={true}
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
