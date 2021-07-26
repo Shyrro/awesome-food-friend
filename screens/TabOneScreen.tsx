6f8928dcf250758366294f26ee5048d9467c18e9
Shyrro <zsahmane@gmail.com>
2021-07-26 17:59:24 +0200

    add calendar
    Signed-off-by: Shyrro <zsahmane@gmail.com>

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,32 +1,81 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import * as React from "react";
+import { StyleSheet } from "react-native";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import EditScreenInfo from "../components/EditScreenInfo";
+import { Calendar, CalendarList, Agenda } from "react-native-calendars";
+import { Text, View } from "../components/Themed";
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
+      <Calendar
+        // Initially visible month. Default = Date()
+        current={"2012-03-01"}
+        // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
+        minDate={"2012-05-10"}
+        // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
+        maxDate={"2012-05-30"}
+        // Handler which gets executed on day press. Default = undefined
+        onDayPress={(day) => {
+          console.log("selected day", day);
+        }}  
+        // Handler which gets executed on day long press. Default = undefined
+        onDayLongPress={(day) => {
+          console.log("selected day", day);
+        }}
+        // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
+        monthFormat={"yyyy MM"}
+        // Handler which gets executed when visible month changes in calendar. Default = undefined
+        onMonthChange={(month) => {
+          console.log("month changed", month);
+        }}
+        // Hide month navigation arrows. Default = false
+        hideArrows={true}
+        // Replace default arrows with custom ones (direction can be 'left' or 'right')
+        renderArrow={(direction) => <Arrow />}
+        // Do not show days of other months in month page. Default = false
+        hideExtraDays={true}
+        // If hideArrows = false and hideExtraDays = false do not switch month when tapping on greyed out
+        // day from another month that is visible in calendar page. Default = false
+        disableMonthChange={true}
+        // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday
+        firstDay={1}
+        // Hide day names. Default = false
+        hideDayNames={true}
+        // Show week numbers to the left. Default = false
+        showWeekNumbers={true}
+        // Handler which gets executed when press arrow icon left. It receive a callback can go back month
+        onPressArrowLeft={(subtractMonth) => subtractMonth()}
+        // Handler which gets executed when press arrow icon right. It receive a callback can go next month
+        onPressArrowRight={(addMonth) => addMonth()}
+        // Disable left arrow. Default = false
+        disableArrowLeft={true}
+        // Disable right arrow. Default = false
+        disableArrowRight={true}
+        // Disable all touch events for disabled days. can be override with disableTouchEvent in markedDates
+        disableAllTouchEventsForDisabledDays={true}
+        // Replace default month and year title with custom one. the function receive a date as parameter
+        renderHeader={(date) => {
+          /*Return JSX*/
+        }}
+        // Enable the option to swipe between months. Default = false
+        enableSwipeMonths={true}
+      />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
